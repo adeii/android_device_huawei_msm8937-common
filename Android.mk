175385af3642d72bd0f114178d5119f9df2a5337
adeii <adeii@dr.com>
2019-10-05 15:28:47 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -26,7 +26,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter diego,$(TARGET_DEVICE)),)
+ifneq ($(filter diego london,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
